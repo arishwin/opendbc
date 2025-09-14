339e7ee169c773a63f49f0f89ad621a39694bcd5
arishwin <arishwinkishaan3@gmail.com>
2025-09-15 00:21:16 +0800

    Refactor Perodua safety hooks: reorganize definitions, fix comments, and align with coding standards.

--- opendbc/safety/modes/perodua.h
+++ opendbc/safety/modes/perodua.h
@@ -18,24 +18,14 @@ static bool perodua_tx_hook(const CANPacket_t *msg) {
   return tx;
 }
 
-static bool perodua_fwd_hook(int bus_num, int addr) {
-  bool block_msg = false;
-
-  if (bus_num == 2) {
-    bool is_lkas_msg = ((addr == 464) || (addr == 628));
-    bool is_acc_msg = ((addr == 625) || (addr == 627));
-    block_msg = (is_lkas_msg || is_acc_msg);
-  }
-  return block_msg;
-}
-
 static safety_config perodua_init(uint16_t param) {
   UNUSED(param);
   static const CanMsg PERODUA_TX_MSGS[] = {
     {464, 0, 8, .check_relay = false},
     {628, 0, 8, .check_relay = false},
     {625, 0, 8, .check_relay = false},
-    {627, 0, 8, .check_relay = false}
+    {627, 0, 8, .check_relay = false},
+    {2015, 0, 8, .check_relay = false}, // 0x7DF
   };
 
   static RxCheck perodua_rx_checks[] = {
@@ -50,5 +40,4 @@ const safety_hooks perodua_hooks = {
   .init = perodua_init,
   .rx   = perodua_rx_hook,
   .tx   = perodua_tx_hook,
-  .fwd  = perodua_fwd_hook,
 };
