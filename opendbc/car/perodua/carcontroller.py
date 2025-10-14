60ac8a918069364f0f7ee80c3a14138516bc274e
arishwin <arishwinkishaan3@gmail.com>
2025-10-14 23:51:13 +0800

    change des_speed to actuators.speed

--- opendbc/car/perodua/carcontroller.py
+++ opendbc/car/perodua/carcontroller.py
@@ -153,7 +153,7 @@ class CarController(CarControllerBase):
     # because dnga is speed controlled, the PID for positive accel is done by the car
     # so we change the equation to v = u + ka and assume k include the time horizon of 1s
     k = 0.3 + 0.06 * CS.out.vEgo
-    des_speed = CS.out.vEgo + actuators.accel * k
+    des_speed = actuators.speed
     apply_brake = 0 if (CS.out.gasPressed or actuators.accel >= 0) else clip(abs(actuators.accel / BRAKE_M), 0., 1.25)
     apply_brake = max(CS.stock_brake_mag * 0.6, apply_brake)
 
